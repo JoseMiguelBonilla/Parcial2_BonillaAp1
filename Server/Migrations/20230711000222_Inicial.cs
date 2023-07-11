04fde04afaa2036110079c00d2a74fd0edd3ade7
Jose Bonilla <josemiguelbonillamar@gmail.com>
2023-07-10 20:53:18 -0400

    preparandome para arreglar el gitignore

---
+++ Server/Migrations/20230711000222_Inicial.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace _2Parcial_BonillaAp1.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Entradas",
+                columns: table => new
+                {
+                    EntradaId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Concepto = table.Column<string>(type: "TEXT", nullable: true),
+                    PesoTotal = table.Column<int>(type: "INTEGER", nullable: false),
+                    ProductoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CantidadProducida = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Entradas", x => x.EntradaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TipoProductos",
+                columns: table => new
+                {
+                    TipoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ProductoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: true),
+                    Existencia = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TipoProductos", x => x.TipoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductosDetalles",
+                columns: table => new
+                {
+                    DetalleId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    EntradaId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ProductoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CantidadUtilizada = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductosDetalles", x => x.DetalleId);
+                    table.ForeignKey(
+                        name: "FK_ProductosDetalles_Entradas_EntradaId",
+                        column: x => x.EntradaId,
+                        principalTable: "Entradas",
+                        principalColumn: "EntradaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TipoProductos",
+                columns: new[] { "TipoId", "Descripcion", "Existencia", "ProductoId" },
+                values: new object[,]
+                {
+                    { 1, "Mani", 0, 0 },
+                    { 3, "Pasas", 0, 0 },
+                    { 4, "Pistacho", 0, 0 },
+                    { 5, "Ciruela", 0, 0 },
+                    { 6, "Arandanos", 0, 0 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductosDetalles_EntradaId",
+                table: "ProductosDetalles",
+                column: "EntradaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductosDetalles");
+
+            migrationBuilder.DropTable(
+                name: "TipoProductos");
+
+            migrationBuilder.DropTable(
+                name: "Entradas");
+        }
+    }
+}
